d944bb254765d8f2863f91f35d4bc754c71c6512
nuigurumi03 <nuigurumi03@gmail.com>
2019-11-25 18:45:31 +0900

    vyu-

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
+<<<<<<< Updated upstream
 
 
   
+=======
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
+>>>>>>> Stashed changes
 end
