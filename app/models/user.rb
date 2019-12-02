a4ad4e67ac269652041436dc328d83478ff5ab9d
nuigurumi03 <nuigurumi03@gmail.com>
2019-12-02 13:04:54 +0900

    インクリメンタルサーチ 実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :messages
   has_many :group_users
-  has_many :groups, through: :group_users       
+  has_many :groups, through: :group_users
+
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
+  
 end
